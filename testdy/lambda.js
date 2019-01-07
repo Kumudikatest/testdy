168ca1a5098eb84fe42939df0562e01039dec631
Kumudikatest <kumudikarupasinghe+adrt@gmail.com>
2019-01-07 17:57:03 +0530

    add dynamodb with put operation

--- testdy/lambda.js
+++ testdy/lambda.js
@@ -2,15 +2,14 @@ let AWS = require('aws-sdk');
 const ddb = new AWS.DynamoDB.DocumentClient();
 
 exports.handler = function (event, context, callback) {
-
-    ddb.query({
-        TableName: 'New'
+    ddb.put({
+        TableName: 'New',
+        Item: { 'ID': 1, 'Name': 'test_name' }
     }).promise().then(function (data) {
         //your logic goes here
     }).catch(function (err) {
         //handle error
     });
-
 
     callback(null, { "message": "Successfully executed" });
 }
