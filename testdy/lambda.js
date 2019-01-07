ed479ff3ee775c0baa865a83ed4341b077bb2006
Kumudikatest <kumudikarupasinghe+adrt@gmail.com>
2019-01-07 17:41:17 +0530

    test6

--- testdy/lambda.js
+++ testdy/lambda.js
@@ -2,14 +2,19 @@ let AWS = require('aws-sdk');
 const ddb = new AWS.DynamoDB.DocumentClient();
 
 exports.handler = function (event, context, callback) {
-    ddb.put({
-        TableName: 'New',
-        Item: { 'Name': 'test_name', 'ID': 1 }
+    ddb.query({
+        TableName: 'New'
+        , ExpressionAttributeValues: { 'Name': 'test_name' }
     }).promise().then(function (data) {
         //your logic goes here
     }).catch(function (err) {
         //handle error
     });
 
+
     callback(null, { "message": "Successfully executed" });
 }
+
+
+
+
