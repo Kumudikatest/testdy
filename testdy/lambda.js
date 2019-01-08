6f821c782954433e974a42e4f55f210a597bc945
Kumudikatest <kumudikarupasinghe+adrt@gmail.com>
2019-01-08 10:48:57 +0530

    new table with query operation

--- testdy/lambda.js
+++ testdy/lambda.js
@@ -2,13 +2,14 @@ let AWS = require('aws-sdk');
 const ddb = new AWS.DynamoDB.DocumentClient();
 
 exports.handler = function (event, context, callback) {
-    ddb.scan({
+    ddb.query({
         TableName: 'New'
     }).promise().then(function (data) {
         //your logic goes here
     }).catch(function (err) {
         //handle error
     });
+
 
     callback(null, { "message": "Successfully executed" });
 }
